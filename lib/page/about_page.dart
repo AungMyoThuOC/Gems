5cf34cb0465478691f486c323e7198f3b387547a
AungMyoThuOC <thua40976@gmail.com>
2022-09-29 09:26:50 +0630

    about

--- lib/page/about_page.dart
+++ lib/page/about_page.dart
@@ -12,9 +12,56 @@ class _AboutState extends State<About> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: const Center(
-        child: Text("About Page"),
-      ),
+      child: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "Gems Recore",
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("What includes :"),
+              const SizedBox(
+                height: 15,
+              ),
+              Column(
+                children: const [
+                  Text(
+                    "-  Can save 8 photos of each gem stone."
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                      "-  Can record the name and the phone number\n  of seller."
+                    ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                      "-  Can record type, weight, accepted date of the\n   gem stone as well as the facts about\n  that stone."
+                    ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                      "-  Can search for recorded gem stones with date,\n   seller's name and gem stone's type."
+                    ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "-  Can edit delete the recorded gem stones."
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      )),
     );
   }
 }
--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -18,3 +18,42 @@ class _HomeState extends State<Home> {
     );
   }
 }
+
+
+// decoration: BoxDecoration(
+//         color: Colors.white,
+//         boxShadow: [
+//           BoxShadow(
+//             color: Colors.grey.withOpacity(0.5),
+//             spreadRadius: 5,
+//             blurRadius: 7,
+//             offset: const Offset(0, 3),
+//           )
+//         ],
+//         borderRadius: const BorderRadius.only(
+//           topLeft: Radius.circular(20.0),
+//           topRight: Radius.circular(20.0),
+//         )
+//       ),
+//       child: Column(
+//         children: [
+//           Stack(
+//             children: [
+//               Positioned(
+//                 right: 10,
+//                 child: SizedBox(
+//                   width: 40,
+//                   child: FloatingActionButton(
+//                     elevation: 0,
+//                     onPressed: (){},
+//                     child: const Icon(
+//                       Icons.add_outlined,
+//                       color: Colors.white,
+//                     ),
+//                   ),
+//                 ),
+//               )
+//             ],
+//           )
+//         ],
+//       ),
