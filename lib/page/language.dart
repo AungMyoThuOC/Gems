676e1b0a315750eca65644b6fb55bf60e14332f5
AungMyoThuOC <thua40976@gmail.com>
2022-09-29 14:27:11 +0630

    language

--- lib/page/language.dart
+++ lib/page/language.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class Language extends StatefulWidget {
@@ -11,10 +13,94 @@ class Language extends StatefulWidget {
 class _LanguageState extends State<Language> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
-        child: Text("Language Page"),
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(100.0),
+        child: AppBar(
+          backgroundColor: Colors.transparent,
+          automaticallyImplyLeading: false,
+          elevation: 0.0,
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(100.0),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: Text("English"),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Divider(),
+                  TextButton(
+                    onPressed: () {},
+                     child: Text("Myanmar")
+                    )
+                ],
+              ),
+            )
+          ),
+        ),
       ),
     );
   }
 }
+
+// Stack(
+//               children: <Widget>[
+//                 Padding(
+//                   padding: const EdgeInsets.all(8.0),
+//                   child: Container(
+//                     width: double.infinity,
+//                     height: double.infinity,
+//                     alignment: Alignment.center,
+//                     decoration: const BoxDecoration(
+//                       gradient: LinearGradient(
+//                         colors: [Colors.white, Colors.white]
+//                       ),
+//                     ),
+//                     child: Padding(
+//                       padding: const EdgeInsets.symmetric(
+//                         horizontal: 10, vertical: 10
+//                       ),
+//                       child: Container(
+//                         width: 350,
+//                         height: 360,
+//                         decoration: BoxDecoration(
+//                           boxShadow: const<BoxShadow> [
+//                             BoxShadow(
+//                               color: Colors.black12,
+//                               blurRadius: 7,
+//                               spreadRadius: 10,
+//                               offset: Offset(0.0, 3.0),
+//                             )
+//                           ],
+//                           borderRadius: BorderRadius.circular(30).copyWith(
+//                             topRight: const Radius.circular(20),
+//                             topLeft: const Radius.circular(20)
+//                           ),
+//                           gradient: const LinearGradient(
+//                             colors: [Colors.white, Colors.white]
+//                           ),
+//                         ),
+//                         child: Container(
+//                           margin: const EdgeInsets.all(20),
+//                           decoration: BoxDecoration(
+//                             borderRadius: BorderRadius.circular(30).copyWith(
+//                               topRight: const Radius.circular(20),
+//                               topLeft: const Radius.circular(20)
+//                             ),
+//                             gradient: const LinearGradient(
+//                               colors: [Colors.white, Colors.white]
+//                             )
+//                           ),
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                 )
+//               ],
+//             )
