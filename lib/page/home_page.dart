57ac41746a937306aba3f0d2fcda108961f3728a
AungMyoThuOC <thua40976@gmail.com>
2022-09-29 11:13:44 +0630

    home

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -9,12 +9,86 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController sercontr = TextEditingController();
+  bool bottomNavigator = true;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
-        child: Text("Home"),
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(80.0),
+        child: AppBar(
+          backgroundColor: Colors.transparent,
+          automaticallyImplyLeading: false,
+          elevation: 0.0,
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(80.0),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                keyboardType: TextInputType.text,
+                controller: sercontr,
+                style: const TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  hintText: "Search With Form Whom",
+                  hintStyle: const TextStyle(
+                    color: Colors.grey,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: const BorderSide(
+                      width: 1,
+                      color: Color.fromARGB(225, 224, 224, 244)
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: const BorderSide(
+                      width: 1,
+                      color: Color.fromARGB(
+                        255, 177, 177, 177
+                      )
+                    )
+                  ),
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.search_outlined,
+                        size: 20,
+                      ),
+                      splashRadius: 2,
+                    ),
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: sercontr.clear,
+                    icon: const Icon(
+                      Icons.clear_outlined,
+                      size: 20,
+                    ),
+                    splashRadius: 2,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
+      floatingActionButton: (bottomNavigator == true)
+          ? SizedBox(
+              width: 45,
+              child: FloatingActionButton(
+                elevation: 0,
+                onPressed: () {},
+                splashColor: Colors.red,
+                child: const Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+              ),
+            )
+          : null,
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
@@ -57,3 +131,4 @@ class _HomeState extends State<Home> {
 //           )
 //         ],
 //       ),
+
